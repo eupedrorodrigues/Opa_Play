0670c0076411e1f325833c3ab62f4d577fa85ea6
Pedro Rodrigues <pedro.rodrigues@compasso.com.br>
2025-03-19 18:40:15 -0300

    chore: initial architecture

---
+++ src/modules/home/pages/index.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+const HomeVideo = () => {
+  return (
+    <>
+      <header className="absolute top-0 left-0 w-full h-[3.75rem] bg-white flex items-center px-4 shadow-md">
+        <h1> Hello World</h1>
+      </header>
+    </>
+  );
+};
+
+export default HomeVideo;
